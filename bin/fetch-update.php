51d13ccb85908adb7d3e57e486ad2944e65a7069
Mponos George <gmponos@gmail.com>
2017-06-20 00:21:35 +0300

    Yaml format (#6)

--- bin/fetch-update.php
+++ bin/fetch-update.php
@@ -2,8 +2,9 @@
 use MoneyPHP\IsoCurrencies\Fetcher;
 use MoneyPHP\IsoCurrencies\Serializer\JsonSerializer;
 use MoneyPHP\IsoCurrencies\Serializer\PhpExportSerializer;
+use MoneyPHP\IsoCurrencies\Serializer\YamlSerializer;
 
-require_once __DIR__."/../vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 
 $fetcher = new Fetcher(
     'http://www.currency-iso.org/dam/downloads/lists/list_one.xml'
@@ -11,3 +12,4 @@ $fetcher = new Fetcher(
 
 $fetcher->saveTo(__DIR__ . "/../resources/current.php", new PhpExportSerializer());
 $fetcher->saveTo(__DIR__ . "/../resources/current.json", new JsonSerializer());
+$fetcher->saveTo(__DIR__ . "/../resources/current.yaml", new YamlSerializer());
